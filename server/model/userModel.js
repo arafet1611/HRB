04d8901215dfec1426cc7b323220cf2d154f4330
arafet <arafetalaya6@gmail.com>
2023-07-03 23:13:38 +0100

    first commit

---
+++ server/model/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  department: {
+    type: String,
+  },
+  jobTitle: {
+    type: String,
+  },
+  experience: {
+    type: Number,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "employee"],
+    default: "employee",
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
